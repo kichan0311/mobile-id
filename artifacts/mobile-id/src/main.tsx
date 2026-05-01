c4deb066d1060184788530801ed091c9bbab07ba
kichan0311 <kichan20080310@gmail.com>
2026-05-01 21:09:00 +0900

    Update main.tsx

--- artifacts/mobile-id/src/main.tsx
+++ artifacts/mobile-id/src/main.tsx
@@ -2,33 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
-// 확대 방지 (두손가락 줌 차단)
-document.addEventListener('gesturestart', (e) => e.preventDefault());
-document.addEventListener('gesturechange', (e) => e.preventDefault());
-document.addEventListener('gestureend', (e) => e.preventDefault());
-
-// 멀티터치 확대 차단
-document.addEventListener(
-  'touchmove',
-  (e) => {
-    if (e.touches.length > 1) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
-
-// PC에서 Ctrl + 휠 확대 차단
-document.addEventListener(
-  'wheel',
-  (e) => {
-    if (e.ctrlKey) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
 }
+
+if ("caches" in window) {
+  caches.keys().then((keys) => {
+    keys.forEach((key) => caches.delete(key));
+  });
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
